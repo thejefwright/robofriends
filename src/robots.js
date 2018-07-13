a74adba77ff92ca80ae7f10771d17a6345fa9d50
jef wright <thejefwright@jefs-MacBook-Pro.local>
2018-07-13 16:29:09 -0400

    initial push of robofriends react course

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Jef Wright',
+    username: 'thejefwright',
+    email: 'jef@jef.pizza'
+  },
+  {
+    id: 2,
+    name: 'Ellis Wright',
+    username: 'theEllisWright',
+    email: 'ellis@jefmadeit.com'
+  },
+  {
+    id: 3,
+    name: 'Melissa Emerson',
+    username: 'Memerson',
+    email: 'Melissa@thejefwright.com'
+  },
+  {
+    id: 4,
+    name: 'Sloane Wright',
+    username: 'theSloaneWright',
+    email: 'Sloane@blackheartdev.com'
+  },
+  {
+    id: 5,
+    name: 'Greta Emerson-Wright',
+    username: 'gretathecat',
+    email: 'Greta@jef.pizza'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
